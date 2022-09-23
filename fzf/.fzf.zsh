2e1f06bcacec51cca4b0f9f716b0550bbbdc14fd
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-09-23 21:46:35 +0530

    Add color theme to fzf

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,3 +1,12 @@
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+--color=dark
+--color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
+--color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
+'
+
 export FZF_DEFAULT_COMMAND="fd . $HOME"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+
+alias fp="fzf --preview 'bat --style numbers,changes --color=always {} | head -500'"
+# bind -x '"\C-o": file="$(fzf --height 40% --reverse)" && [ -f "$file" ] && xdg-open "$file"'
