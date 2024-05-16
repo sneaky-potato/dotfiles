24ee4f00757b7ab1e590fd1e57207720816402f1
sneaky-potato <ashwanikamal.im421@gmail.com>
2024-05-16 19:38:49 +0530

    REVAMP: moved to arch linux 6.8.9

---
+++ sxhkd/volume_brightness.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+notification_timeout=1000
+volume_step=1
+max_volume=100
+
+function get_volume {
+    pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '[0-9]{1,3}(?=%)' | head -1
+}
+
+function get_mute {
+    pactl get-sink-mute @DEFAULT_SINK@ | grep -Po '(?<=Mute: )(yes|no)'
+}
+
+function get_brightness {
+    curr_val=$(brightnessctl g)
+    max_val=$(brightnessctl m)
+    echo $((curr_val*100 / max_val))
+}
+
+function get_brightness_icon {
+    brightness_icon=""
+}
+
+
+function get_volume_icon {
+    volume=$(get_volume)
+    mute=$(get_mute)
+    if [ "$volume" -eq 0 ] || [ "$mute" == "yes" ] ; then
+        volume_icon=""
+    elif [ "$volume" -lt 50 ]; then
+        volume_icon=""
+    else
+        volume_icon=""
+    fi
+}
+
+function show_volume_notif {
+    volume=$(get_mute)
+    get_volume_icon
+    notify-send -t $notification_timeout -h string:x-dunst-stack-tag:volume_notif -h int:value:$volume "$volume_icon $volume%"
+}
+
+function show_brightness_notif {
+    brightness=$(get_brightness)
+    echo $brightness
+    get_brightness_icon
+    notify-send -t $notification_timeout -h string:x-dunst-stack-tag:brightness_notif -h int:value:$brightness "$brightness_icon $brightness%"
+}
+
+case $1 in
+    volume_up)
+    # Unmutes and increases volume, then displays the notification
+    pactl set-sink-mute @DEFAULT_SINK@ 0
+    volume=$(get_volume)
+    if [ $(( "$volume" + "$volume_step" )) -gt $max_volume ]; then
+        pactl set-sink-volume @DEFAULT_SINK@ $max_volume%
+    else
+        pactl set-sink-volume @DEFAULT_SINK@ +$volume_step%
+    fi
+    show_volume_notif
+    ;;
+
+    volume_down)
+    # Raises volume and displays the notification
+    pactl set-sink-volume @DEFAULT_SINK@ -$volume_step%
+    show_volume_notif
+    ;;
+
+    volume_mute)
+    # Toggles mute and displays the notification
+    pactl set-sink-mute @DEFAULT_SINK@ toggle
+    show_volume_notif
+    ;;
+
+    brightness_up)
+    # Increases brightness and displays the notification
+    brightnessctl s 9600+ 
+    show_brightness_notif
+    ;;
+
+    brightness_down)
+    # Decreases brightness and displays the notification
+    brightnessctl s 9600-
+    show_brightness_notif
+    ;;
+
+esac
