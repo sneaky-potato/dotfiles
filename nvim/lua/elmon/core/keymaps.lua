89bd20a41679574d025b8249fc6f1d6960bf6047
shawn <ashwanikamal.im421@gmail.com>
2024-12-23 14:10:25 +0530

    [nvim] Add global Map function for seeting keymaps

--- nvim/lua/elmon/core/keymaps.lua
+++ nvim/lua/elmon/core/keymaps.lua
@@ -1,11 +1,23 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+function Map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
 
-vim.keymap.set("n", "<A-j>", ":m .+1<cr>==")
-vim.keymap.set("n", "<A-k>", ":m .-2<cr>==")
-vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<cr>==gi")
-vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<cr>==gi")
-vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv")
-vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv")
-vim.keymap.set("n", "<C-TAB>", "gt")
-vim.keymap.set("n", "<C-S-TAB>", "gT")
+vim.g.mapleader = " "
+
+-- Deprecated since not using netrw anymore for browsing
+-- vim.keymap.set("n", "<leader>pv", ":Ex")
+
+Map("n", "<A-j>", ":m .+1<cr>==")
+Map("n", "<A-k>", ":m .-2<cr>==")
+Map("i", "<A-j>", "<Esc>:m .+1<cr>==gi")
+Map("i", "<A-k>", "<Esc>:m .-2<cr>==gi")
+Map("v", "<A-j>", ":m '>+1<cr>gv=gv")
+Map("v", "<A-k>", ":m '<-2<cr>gv=gv")
+Map("n", "<C-h>", "<C-w>h")
+Map("n", "<C-j>", "<C-w>j")
+Map("n", "<C-k>", "<C-w>k")
+Map("n", "<C-l>", "<C-w>l")
