e1ed8e60a146cc620dd79f788e6bc9eac8a7d89f
shawn <ashwanikamal.im421@gmail.com>
2024-12-23 14:34:57 +0530

    [nvim] Add keymaps for horizontal movement of code

--- nvim/lua/elmon/core/keymaps.lua
+++ nvim/lua/elmon/core/keymaps.lua
@@ -11,13 +11,21 @@ vim.g.mapleader = " "
 -- Deprecated since not using netrw anymore for browsing
 -- vim.keymap.set("n", "<leader>pv", ":Ex")
 
+-- Easy horizontal movement of code through file
+Map("v", "<", "<gv")
+Map("v", ">", ">gv")
+
+-- Move code anywhere through the file
 Map("n", "<A-j>", ":m .+1<cr>==")
 Map("n", "<A-k>", ":m .-2<cr>==")
 Map("i", "<A-j>", "<Esc>:m .+1<cr>==gi")
 Map("i", "<A-k>", "<Esc>:m .-2<cr>==gi")
 Map("v", "<A-j>", ":m '>+1<cr>gv=gv")
 Map("v", "<A-k>", ":m '<-2<cr>gv=gv")
+
+-- Select any window using C-hjkl
 Map("n", "<C-h>", "<C-w>h")
 Map("n", "<C-j>", "<C-w>j")
 Map("n", "<C-k>", "<C-w>k")
 Map("n", "<C-l>", "<C-w>l")
+
